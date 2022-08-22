60ee55899e498581c026e635ab47491e6a46722d
wdssmq <wdssmq@qq.com>
2022-08-22 15:04:51 +0800

    test. 增加测试用例

---
+++ test/gm/src/__info.js
@@ -0,0 +1,29 @@
+const gm_banner = `
+// ==UserScript==
+// @name         New Userscript
+// @namespace    https://www.wdssmq.com/
+// @version      0.1
+// @author       沉冰浮水
+// @description  try to take over the world!
+// @license      MIT
+// @null         ----------------------------
+// @contributionURL    https://github.com/wdssmq#%E4%BA%8C%E7%BB%B4%E7%A0%81
+// @contributionAmount 5.93
+// @null         ----------------------------
+// @link         https://github.com/wdssmq/userscript
+// @link         https://afdian.net/@wdssmq
+// @link         https://greasyfork.org/zh-CN/users/6865-wdssmq
+// @null         ----------------------------
+// @noframes
+// @run-at       document-end
+// @match        http://127.0.0.1:3000/
+// @grant        none
+// ==/UserScript==
+
+/* eslint-disable */
+/* jshint esversion: 6 */
+`
+
+const gm_name = "gm"
+
+export { gm_banner, gm_name }
