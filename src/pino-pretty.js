e4cf2fd6389cf80a9003da58c529800d6c1720a4
wdssmq <wdssmq@qq.com>
2022-08-22 14:57:51 +0800

    up. 日志输出接口

--- src/pino-pretty.js
+++ src/pino-pretty.js
@@ -2,6 +2,7 @@ import pinoPretty from 'pino-pretty'
 import { bold, green, blue } from 'femtocolor'
 
 const header = blue('⚡︎dev-server')
+const _url = (url) => bold(green(url))
 
 export default opts => pinoPretty({
   ...opts,
@@ -11,8 +12,13 @@ export default opts => pinoPretty({
     if (log.msg) {
       if (log.msg.includes('Server listening')) {
         const startup = log.msg.replace('Server listening', header).split('at')
-        log.msg = startup[0] + 'listening on' + bold(green(startup[1]))
+        log.msg = startup[0] + 'listening on' + _url(startup[1])
       }
+      log.msg = log.msg.replace('{{url}}', _url(log.url))
+      log.msg = log.msg.replace('{{header}}', header)
+      // 删除字段
+      delete log.url
+      delete log.reqId
       return log.msg
     }
   }
