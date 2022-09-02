f8c7ded0617a227aa7ec48dd1dc18dc2c838fa5d
wdssmq <wdssmq@qq.com>
2022-09-02 15:13:14 +0800

    doc. debug 用的命令优化（默认注释）

--- src/base.js
+++ src/base.js
@@ -4,15 +4,12 @@ import { fileURLToPath } from 'url'
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 
-// console.log(__dirname, __filename)
-// console.log(path.resolve(__dirname, "../"))
-
 const monkeyPath = {
   base: path.resolve(__dirname, '../'),
   liveJS: path.resolve(__dirname, '../node_modules/livereload-js/dist/livereload.js'),
 }
 
-// console.log(monkeyPath)
+// console.log(Object.assign({}, { __dirname, __filename }, monkeyPath))
 
 export default monkeyPath
 
