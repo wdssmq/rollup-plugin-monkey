d5d3f7d647cb0ad47f037fc36ca9dc6f2c23eccc
wdssmq <wdssmq@qq.com>
2022-09-02 16:39:16 +0800

    chore. #pinoPretty 复制到 dist 目录

--- rollup.config.js
+++ rollup.config.js
@@ -1,10 +1,19 @@
 import pkg from './package.json'
 
-export default {
-  input: 'src/main.js',
-  output: [
-    { file: pkg.main, format: 'cjs', exports: 'default' },
-    { file: pkg.module, format: 'esm', banner: '/* eslint-disable */' }
-  ],
-  external: ['path', ...Object.keys(pkg.dependencies)]
-}
+export default [
+  {
+    input: 'src/main.js',
+    output: [
+      { file: pkg.main, format: 'cjs', exports: 'default' },
+      { file: pkg.module, format: 'esm', banner: '/* eslint-disable */' }
+    ],
+    external: ['path', ...Object.keys(pkg.dependencies)]
+  },
+  {
+    input: 'src/pino-pretty.js',
+    output: [
+      { file: pkg.imports['#pinoPretty'], format: 'esm', banner: '/* eslint-disable */' }
+    ],
+    external: ['path', ...Object.keys(pkg.dependencies)]
+  }
+]
