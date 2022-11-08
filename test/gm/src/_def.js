c049fde278e4b3f3586e0361ef987063edcbbcce
wdssmq <wdssmq@qq.com>
2022-11-08 15:39:34 +0800

    test:gm. 2022-11-08 - 增加全局存储封装； - 修改 _def 示例；

--- test/gm/src/_def.js
+++ test/gm/src/_def.js
@@ -1,5 +1,14 @@
-import { _log } from "./_base";
+import { _log, $n } from "./_base";
+
+// 一个全局数据存储封装，带 ls 读写
+import { gob } from "./_gob.js";
+_log("[TEST]gob.data", gob.data);
+
 // Your code here...
-_log("Hello, world!");
-alert("Hello, world!");
+_log("[TEST]Hello, world!");
+if (gob.intTest <= 3) {
+  alert("[TEST]Hello, world!");
+}
+const $p = $n("p");
+$p.innerHTML += `<br>${gob.strTest}`;
 // Your code here...
