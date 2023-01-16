0601a5ab18b07925e98af218c9d955caef4127ba
wdssmq <wdssmq@qq.com>
2023-01-16 14:22:48 +0800

    chore. 配置项改为变量

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,13 @@
 import pkg from './package.json'
 
 const banner = '/* eslint-disable */'
+const external = [
+  'fs',
+  'node:module',
+  'path',
+  'url',
+  ...Object.keys(pkg.dependencies)
+]
 
 export default [
   {
@@ -9,20 +16,20 @@ export default [
       { file: pkg.main, format: 'cjs', banner, exports: 'named' },
       { file: pkg.module, format: 'esm', banner }
     ],
-    external: ['path', ...Object.keys(pkg.dependencies)]
+    external
   },
   {
     input: 'src/__pino-pretty.js',
     output: [
       { file: pkg.imports['#pinoPretty'], format: 'esm', banner }
     ],
-    external: ['path', ...Object.keys(pkg.dependencies)]
+    external
   },
   {
     input: 'src/__dev.js',
     output: [
       { file: pkg.imports['#dev'], format: 'esm', banner }
     ],
-    external: ['path', ...Object.keys(pkg.dependencies)]
+    external
   }
 ]
