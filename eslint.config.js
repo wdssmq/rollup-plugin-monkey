7ae5ef30a3df851eb9a6fac9adcbad79953c7609
wdssmq <wdssmq@qq.com>
2026-07-11 01:58:01 +0800

    chore. eslint 规则改为 @antfu/eslint-config，安装依赖并移除旧的规则文件；

---
+++ eslint.config.js
@@ -0,0 +1,33 @@
+import antfu from '@antfu/eslint-config'
+
+export default antfu(
+  {
+    languageOptions: {
+      globals: { placeholder: false, $: false },
+    },
+    rules: {
+      '@/func-call-spacing': 'error',
+      'no-console': 'off',
+      'style/arrow-spacing': 'error',
+      'style/brace-style': ['error', '1tbs', { allowSingleLine: true }],
+      'style/comma-dangle': ['warn', 'always-multiline'],
+      'style/indent': ['error', 2, { SwitchCase: 1 }],
+      'style/keyword-spacing': 'error',
+      'style/quotes': ['error', 'single'],
+      'style/semi': ['error', 'never'],
+      'style/space-before-blocks': 'error',
+      'style/space-infix-ops': 'error',
+    },
+  },
+  {
+    files: ['test/**/*.{js,mjs}'],
+    rules: {
+      'antfu/no-import-dist': 'off',
+      'no-alert': 'off',
+      'style/comma-dangle': ['warn', 'always-multiline'],
+      'style/quotes': ['error', 'double'],
+      'style/semi': ['error', 'always', { omitLastInOneLineBlock: true }],
+      // 'style/quote-props': 'off',
+    },
+  },
+)
